826152fa7c9c4052c22873bc7874df6156f46874
Amado Dubon <desshinra@gmail.com>
2023-11-14 16:08:27 -0600

    add: valor carta

--- assets/js/game.js
+++ assets/js/game.js
@@ -66,3 +66,28 @@ const pedirCarta = () => {
 
 pedirCarta();
 
+const valorCarta = ( carta ) => {
+    
+    const valor = carta.substring(0, carta.length - 1);
+    return (isNaN(valor)) ?
+        (valor === 'A') ? 11 : 10
+        : valor * 1;
+
+
+
+    // let puntos = 0;
+    // console.log({ valor });
+    
+    // if ( isNaN(valor)) {
+        
+    //     puntos = (valor === 'A') ? 11 : 10;
+
+    // } else {
+    //     puntos = valor * 1;
+    // }
+
+    // console.log(puntos )
+
+}
+
+valorCarta('JD');
